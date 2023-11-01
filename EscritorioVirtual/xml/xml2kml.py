d0b92aa94d047d9b62629669421f21efaa45d00d
Patricia García <uo282210@uniovi.es>
2023-11-01 13:22:17 +0100

    avances en p2 de xml

---
+++ EscritorioVirtual/xml/xml2kml.py
@@ -0,0 +1,92 @@
+import xml.etree.ElementTree as ET
+
+
+def prologoKML(archivo, nombre):
+    """ Escribe en el archivo de salida el prólogo del archivo KML"""
+
+    archivo.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+    archivo.write('<kml xmlns="http://www.opengis.net/kml/2.2">\n')
+    archivo.write("<Document>\n")
+    archivo.write("<Placemark>\n")
+    archivo.write("<name>"+nombre+"</name>\n")    
+    archivo.write("<LineString>\n")
+    #la etiqueta <extrude> extiende la línea hasta el suelo 
+    archivo.write("<extrude>1</extrude>\n")
+    # La etiqueta <tessellate> descompone la línea en porciones pequeñas
+    archivo.write("<tessellate>1</tessellate>\n")
+    archivo.write("<coordinates>\n")
+
+def epilogoKML(archivo):
+    """ Escribe en el archivo de salida el epílogo del archivo KML"""
+
+    archivo.write("</coordinates>\n")
+    archivo.write("<altitudeMode>relativeToGround</altitudeMode>\n")
+    archivo.write("</LineString>\n")
+    archivo.write("<Style> id='lineaRoja'>\n") 
+    archivo.write("<LineStyle>\n") 
+    archivo.write("<color>#ff0000ff</color>\n")
+    archivo.write("<width>5</width>\n")
+    archivo.write("</LineStyle>\n")
+    archivo.write("</Style>\n")
+    archivo.write("</Placemark>\n")
+    archivo.write("</Document>\n")
+    archivo.write("</kml>\n")
+
+def main():
+    """Procesado de archivos xml con coordenadas y generación de un archivo KML (Keyhole Markup Language)
+KML es un formato de archivo que se utiliza para mostrar datos geográficos en un navegador terrestre.
+Se utiliza por Google Earth, Google Maps y Google Maps para móviles.
+KML utiliza una estructura basada en etiquetas con atributos y elementos anidados y está basado en el estándar XML
+
+Versión 1.0 31/Octubre/2023
+Patricia García Fernández. Universidad de Oviedo
+
+    """
+    print(main.__doc__)
+    nombreArchivo = input("Introduzca el nombre del archivo xml    = ")
+
+    try:
+        archivo = open(nombreArchivo,'r')
+    except IOError:
+        print ('No se encuentra el archivo ', nombreArchivo)
+        exit()
+
+    root = ET.parse(nombreArchivo).getroot()
+
+    rutas = root.findall('.//{http://www.uniovi.es}ruta')
+    # Procesamiento y generación del archivo kml    
+    for ruta in rutas:
+        
+        nombreSalida  = input("Introduzca el nombre del archivo generado (*.kml) = ")
+        try:
+            salida = open(nombreSalida + ".kml",'w')
+        except IOError:
+            print ('No se puede crear el archivo ', nombreSalida + ".kml")
+            exit()
+
+        # Escribe la cabecera del archivo de salida
+        prologoKML(salida, nombreArchivo)
+
+        coordenadas = ruta.find('.//{http://www.uniovi.es}coordenadas')
+        longitud = coordenadas.get('longitud')
+        latitud = coordenadas.get('latitud')
+        altitud = coordenadas.get('altitud')
+        salida.write(latitud + "," + longitud + "," + altitud + "\n")
+
+        hitos = ruta.findall('.//{http://www.uniovi.es}hitos/{http://www.uniovi.es}hito')
+        for hito in hitos:
+            coordenadas = hito.find('.//{http://www.uniovi.es}coordenadas')
+            longitud = coordenadas.get('longitud')
+            latitud = coordenadas.get('latitud')
+            altitud = coordenadas.get('altitud')
+            salida.write(latitud + "," + longitud + "," + altitud + "\n")
+
+        epilogoKML(salida)
+        
+        salida.close()
+    archivo.close()
+
+    # Esribe el epilogo del archivo de salida
+
+if __name__ == "__main__":
+    main()
---
+++ EscritorioVirtual/xml/xml2perfil.py
@@ -0,0 +1,92 @@
+import xml.etree.ElementTree as ET
+
+
+def prologoSVG(archivo, nombre):
+    """ Escribe en el archivo de salida el prólogo del archivo SVG para crear una polilyne"""
+
+    archivo.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+    archivo.write('<svg xmlns="http://www.w3.org/2000/svg" version="2.0">\n')
+    archivo.write('<polyline points = "')
+
+def epilogoPolilineaSVG(archivo):
+    """ Escribe en el archivo de salida el epílogo del archivo SVG que crea una polilyne"""
+    archivo.write('"\n')
+    archivo.write('style="fill:white;stroke:red;stroke-width=4" />\n')
+
+def epilogoSVG(archivo):
+    """ Escribe en el archivo de salida el epílogo de la polilinea"""
+    archivo.write("</svg>\n")
+
+def textoSVG(archivo, x, y, texto):
+    archivo.write('<text x="' + x + '" y="' + y + '" style="writing-mode: tb; glyph-orientation-vertical: 0;">\n')
+    archivo.write(texto + "\n")
+    archivo.write('</text>\n')
+
+def main():
+    """Procesado de archivos xml con coordenadas y generación de un archivo SVG (Scalable Vector Graphics)
+
+Versión 1.0 31/Octubre/2023
+Patricia García Fernández. Universidad de Oviedo
+    """
+    print(main.__doc__)
+    nombreArchivo = input("Introduzca el nombre del archivo xml    = ")
+
+    try:
+        archivo = open(nombreArchivo,'r')
+    except IOError:
+        print ('No se encuentra el archivo ', nombreArchivo)
+        exit()
+
+    root = ET.parse(nombreArchivo).getroot()
+
+    rutas = root.findall('.//{http://www.uniovi.es}ruta')
+    # Procesamiento y generación del archivo kml    
+    for ruta in rutas:
+        
+        nombreSalida  = input("Introduzca el nombre del archivo generado (*.svg) = ")
+        try:
+            salida = open(nombreSalida + ".svg",'w')
+        except IOError:
+            print ('No se puede crear el archivo ', nombreSalida + ".kml")
+            exit()
+
+        # Escribe la cabecera del archivo de salida
+        prologoSVG(salida, nombreArchivo)
+
+        #Escribe la polilinea
+        comienzoPoligonoX = 100
+        coordenadas = ruta.find('.//{http://www.uniovi.es}coordenadas')
+        comienzoAltitud = "150" #coordenadas.get('altitud')
+        salida.write(str(comienzoPoligonoX) + "," + comienzoAltitud + "\n")
+
+        hitos = ruta.findall('.//{http://www.uniovi.es}hitos/{http://www.uniovi.es}hito')
+        incrementoX = comienzoPoligonoX
+        for hito in hitos:
+            coordenadas = hito.find('.//{http://www.uniovi.es}coordenadas')
+            altitud = coordenadas.get('altitud')
+            distancia = hito.find('.//{http://www.uniovi.es}distancia-hito-anterior')
+            incrementoX += float(distancia.text)*100
+            salida.write(str(incrementoX) + "," + str(float(comienzoAltitud) - float(altitud)/10) + "\n")
+        salida.write(str(incrementoX+30) + "," + comienzoAltitud + "\n")
+        salida.write(str(comienzoPoligonoX) + "," + comienzoAltitud)
+        epilogoPolilineaSVG(salida)
+
+        comienzoTexto = "155"
+        #Escribe el texto de la polilinea de cada hito
+        textoSVG(salida, str(comienzoPoligonoX), comienzoTexto, "Inicio")
+        incrementoX = comienzoPoligonoX
+        for hito in hitos:
+            nombre = hito.find('.//{http://www.uniovi.es}nombre')
+            distancia = hito.find('.//{http://www.uniovi.es}distancia-hito-anterior')
+            incrementoX += float(distancia.text)*100
+            textoSVG(salida, str(incrementoX), comienzoTexto, nombre.text)
+        textoSVG(salida, str(incrementoX+30), comienzoTexto, "Final")
+
+        epilogoSVG(salida)
+        salida.close()
+    archivo.close()
+
+    # Esribe el epilogo del archivo de salida
+
+if __name__ == "__main__":
+    main()
