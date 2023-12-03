3c11b01800cc0acd30c736e0eb7302a9bea5214f
garciafdezpatricia <uo282210@uniovi.es>
2023-12-03 16:21:02 +0100

    p8 acabada

---
+++ EscritorioVirtual/js/api.js
@@ -0,0 +1,154 @@
+class ListaTareas {
+
+    constructor() {
+        this.tasks = [];
+        this.tareaAMover = null;
+        this.loadFromStorage()
+    }
+
+    loadFromStorage() {
+        if (localStorage.getItem('tasks')){
+            this.tasks = JSON.parse(localStorage.getItem('tasks'));
+            for (let i = 0; i < this.tasks.length; i++) {
+                let contenido = this.tasks[i];
+                let article = $("<article>").attr("data-element", "tarea").attr('draggable', 'true');
+                let title = $("<p>").text(contenido[0]).attr("data-element", "titulo");
+                article.append(title);
+                let text = $("<p>").text(contenido[1]).attr("data-element", "texto");
+                article.append(text);
+                let tipo = contenido[2];
+                if (tipo.toUpperCase() === "TO-DO"){
+                    article.attr("data-state", "to-do");
+                    $('section[data-element="to-do"]').append(article);
+                }
+                else if (tipo.toUpperCase() === "IN-PROGRESS"){
+                    article.attr("data-state", "in-progress");
+                    $('section[data-element="in-progress"]').append(article);
+                }
+                else {
+                    article.attr("data-state", "done");
+                    $('section[data-element="done"]').append(article);
+                }
+            }
+            this.addListeners();
+        }
+    }
+
+    loadTasks(file) {
+        let archivo = file[0];
+        var tipoTexto = /text.*/;
+        if (archivo.type.match(tipoTexto)) {
+            var lector = new FileReader();
+            lector.onload = this.onFileLoad.bind(this, this.tasks);
+            lector.readAsText(archivo);            
+        }
+        else {
+            errorArchivo.innerText = "Error : ¡¡¡ Archivo no válido !!!";
+        }       
+    }
+
+    onFileLoad(tasks, evento) {
+        let contenidoArchivo = evento.target.result;
+        let counter = 1;
+        for (let tarea of contenidoArchivo.split("\n")){
+            tarea = tarea.trim();
+            let article = $("<article>").attr("data-element", "tarea").attr('draggable', 'true');
+            let contenido = tarea.split("_");
+            let title = $("<p>").text(contenido[0]).attr("data-element", "titulo");
+            article.append(title);
+            let text = $("<p>").text(contenido[1]).attr("data-element", "texto");
+            article.append(text);
+            let tipo = contenido[2];
+            if (tipo === "TO-DO"){
+                article.attr("data-state", "to-do");
+                $('section[data-element="to-do"]').append(article);
+            }
+            else if (tipo === "IN-PROGRESS"){
+                article.attr("data-state", "in-progress");
+                $('section[data-element="in-progress"]').append(article);
+            }
+            else {
+                article.attr("data-state", "done");
+                $('section[data-element="done"]').append(article);
+            }
+            let t = [contenido[0], contenido[1], contenido[2]]
+            this.tasks.push(t);
+            counter += 1;
+        }
+        // añadir listeners para el drag and drop
+        this.addListeners();
+        // guardar las tareas en el web storage
+        this.saveTasks();
+    }
+
+    addListeners() {
+        let tareas = $('article[data-element="tarea"]');
+        tareas.each((index, article) => {
+            $(article).on('dragstart', this.dragStart.bind(this));
+        });
+
+        let todo = $('section[data-element="to-do"]');
+        $(todo).on('dragover', this.allowDrop.bind(this))
+        $(todo).on('drop', this.drop.bind(this));
+
+        let progress = $('section[data-element="in-progress"]');
+        $(progress).on('dragover', this.allowDrop.bind(this))
+        $(progress).on('drop', this.drop.bind(this));
+
+        let done = $('section[data-element="done"]');
+        $(done).on('dragover', this.allowDrop.bind(this));
+        $(done).on('drop', this.drop.bind(this));
+    }
+
+    saveTasks() {
+        // API de Web Storage
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    }
+
+    allowDrop(event) {
+        event.preventDefault();
+    }
+
+    dragStart(event) {
+        // datos que se transferirán durante la operación de arrastrar
+        const estado = event.target.dataset.state;
+        this.tareaAMover = event.target;
+
+        event.originalEvent.dataTransfer.setData("text/plain", JSON.stringify({
+            state: estado
+        }));
+    }
+
+    drop(event) {
+        event.preventDefault();
+        
+        const nuevaSeccion = $(event.target);
+        // Cambiar el estado de la tarea
+        $(this.tareaAMover).attr("data-state", nuevaSeccion.attr("data-element"))
+
+        // Mueve la tarea a la sección correspondiente
+        nuevaSeccion.append(this.tareaAMover);
+
+        // Actualiza la información en el almacenamiento local
+        this.updateTasks();
+        
+    }
+
+    updateTasks() {
+        this.tasks = [];
+
+        let tareas = $('article[data-element="tarea"]');
+        tareas.each((index, tarea ) => {
+            let nodoTarea = $(tarea);
+            const taskInfo = [
+                nodoTarea.find('p[data-element="titulo"]').text(),
+                nodoTarea.find('p[data-element="texto"]').text(),
+                nodoTarea.attr('data-state')
+            ];
+            this.tasks.push(taskInfo);
+        })
+
+        // Guarda las tareas actualizadas
+        this.saveTasks();
+    }
+}
--- EscritorioVirtual/js/viajes.js
+++ EscritorioVirtual/js/viajes.js
@@ -1,6 +1,8 @@
 class Viajes {
 
     apikey = "AIzaSyBOHT5uUmRE7AYHojYkyoz8xALNtuLMAF4";
+    mapaGeoposicionado;
+    colorIndex = 0;
 
     constructor() {
         navigator.geolocation.getCurrentPosition(this.procesarPosicion.bind(this), this.verErrores.bind(this));
@@ -64,7 +66,7 @@ class Viajes {
     getMapaDinamicoGoogle(){    
         // las coordenadas son fijas porque si se ponen las del objeto igual no estan cargadas todavia
         let posicion = { lat: 43.3634472, lng: -5.8459444}
-        var mapaGeoposicionado = new google.maps.Map(document.getElementById('dinamico'), {
+        this.mapaGeoposicionado = new google.maps.Map(document.getElementById('dinamico'), {
             zoom: 10,
             center: posicion,
             mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -73,7 +75,54 @@ class Viajes {
         let infoWindow = new google.maps.InfoWindow;
         infoWindow.setPosition(posicion);
         infoWindow.setContent('Ubicación actual');
-        infoWindow.open(mapaGeoposicionado);
+        infoWindow.open(this.mapaGeoposicionado);
+    }
+
+    leerArchivoKML(files) {
+        for (let i = 0; i < files.length; i++){
+            let file = files[i];
+            let lector = new FileReader();
+            lector.onload = this.onFileLoad.bind(this, this.mapaGeoposicionado);
+            lector.readAsText(file);
+        }
+    }
+
+    onFileLoad(mapa, evento){
+        const colores = ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#FF00FF'];
+        let kml = $(evento.target.result);
+        let coordinates = kml.find('coordinates').text().trim().split('\n');
+        let coordinatesArray = []
+        let nuevoCentro;
+        for (let i = 0; i < coordinates.length; i++){
+            let coord = coordinates[i];
+            let [lng, lat, alt] = coord.split(",").map(parseFloat);
+            coordinatesArray.push({lat, lng, alt});
+            nuevoCentro = {lat: lat, lng: lng};
+        }
+        let color = colores[this.colorIndex % colores.length];
+        this.colorIndex += 1;
+        let ruta = new google.maps.Polyline({
+            path: coordinatesArray,
+            geodesic: true,
+            strokeColor: color,
+            strokeOpacity: 1.0,
+            strokeWeight: 5
+        })
+        ruta.setMap(this.mapaGeoposicionado);
+        mapa.setCenter(nuevoCentro);
+    }
+
+
+    leerArchivoSVG(files) {
+        for (let i = 0; i < files.length; i++){
+            let file = files[i];
+            let lector = new FileReader();
+            lector.onload = function (evento) {
+                let svg = $(lector.result);
+                $("section[data-element='archivosSVG']").append(svg);
+            }
+            lector.readAsText(file);
+        }
     }
 
     leerArchivoXML(files) {
@@ -81,41 +130,114 @@ class Viajes {
         {  
             //El navegador soporta el API File
             let archivo = files[0];    
+            if (archivo) {
+                let lector = new FileReader();
+                lector.readAsText(archivo);
+                lector.onload = function(evento) {
+                    let xml = $(lector.result);
+                    if ($('ruta', xml).length > 0){
+                        let titleRutas = $("<h3>").text("Rutas");
+                        $("section[data-element='rutas']").append(titleRutas);
+                    }
+                    $('ruta', xml).each( (index, ruta) => {
+                        const rutaXML = $(ruta);
+            
+                        let article = $("<article>").attr("data-element", "infoRuta");
+        
+                        let nombre = $("<h4>").text(`Ruta ${index+1}: ${rutaXML.find('nombre').first().text()}`).attr("data-element", "nombre-ruta");
+                        article.append(nombre);
 
+                        let tipo = $("<p>").text(`Tipo: ${rutaXML.attr("tipo")}`).attr("data-element", "tipo-ruta");
+                        article.append(tipo);
 
-            $('ruta', archivo).each( (index, ruta) => {
-                const rutaXML = $(ruta);
-    
-                let article = $("<article>");
+                        let descripcion = $("<p>").text(rutaXML.find('descripcion').first().text()).attr("data-element", "descripcion-ruta");
+                        article.append(descripcion);
 
-                let tipo = $("<p>").text(rutaXML.attr("tipo").text());
-                let nombre = $("<p>").text(rutaXML.find('nombre').first().text());
-                let fecha_inicio = $("<p>").text(rutaXML.find('fecha-inicio').text());
-                let hora_inicio = $("<p>").text(rutaXML.find('hora-inicio').text());
-                let medio_transporte = $("<p>").text(rutaXML.find('medio-transporte').text());
-                let tiempo_duracion = $("<p>").text(rutaXML.find('tiempo-duracion').text());
-                let agencia = $("<p>").text(rutaXML.find('agencia').text());
-                let descripcion = $("<p>").text(rutaXML.find('descripcion').text());
-                let personas_adecuadas = $("<p>").text(rutaXML.find('personas-adecuadas').text());
-                let lugar_inicio = $("<p>").text(rutaXML.find('lugar-inicio').text());
-                let longitud = $("<p>").text(rutaXML.find('coordenadas').attr('longitud')); 
-                let latitud = $("<p>").text(rutaXML.find('coordenadas').attr('latitud'));
-                let altitud = $("<p>").text(rutaXML.find('coordenadas').attr('altitud'));  
-                let referencias = [];
-                let recomendacion = $("<p>").text(rutaXML.find('recomendacion').text());
-                let hitos = [];
+                        let tiempo_duracion = $("<p>").text(`Duración: ${rutaXML.find('tiempo-duracion').text()}`).attr("data-element", "tiempo-duracion-ruta");
+                        article.append(tiempo_duracion);
 
-                rutaXML.find('referencias').find('referencia').each((index, ref) => {
-                    const referencia = $(ref);
-                    referencias[index] = referencia.text();
-                })
+                        let personas_adecuadas = $("<p>").text(`Personas adecuadas: ${rutaXML.find('personas-adecuadas').text()}`).attr("data-element", "personas-ruta");
+                        article.append(personas_adecuadas);
 
-                rutaXML.find('hitos').find('hito').each((index, hito) => {
-                    const hito = $(hito);
-                    // como almaceno todo? -> ul?
-                })
-    
-            });
+                        let lugar_inicio = $("<p>").text(`Lugar de inicio: ${rutaXML.find('lugar-inicio').text()}`).attr("data-element", "lugar-inicio-ruta");
+                        article.append(lugar_inicio);
+
+                        let fecha_inicio = $("<p>").text(`Fecha inicio: ${rutaXML.find('fecha-inicio').text()}`).attr("data-element", "fecha-inicio-ruta");
+                        article.append(fecha_inicio);
+
+                        let hora_inicio = $("<p>").text(`Hora inicio: ${rutaXML.find('hora-inicio').text()}`).attr("data-element", "hora-inicio-ruta");
+                        article.append(hora_inicio);
+
+                        let medio_transporte = $("<p>").text(`Medio transporte: ${rutaXML.find('medio-transporte').text()}`).attr("data-element", "medio-transporte-ruta");
+                        article.append(medio_transporte);
+
+                        let agencia = $("<p>").text(`Agencia: ${rutaXML.find('agencia').text()}`).attr("data-element", "agencia-ruta");
+                        article.append(agencia);
+
+                        let coordenadas = $("<p>").text(`Coordenadas: ${rutaXML.find('coordenadas').attr('longitud')}, ${rutaXML.find('coordenadas').attr('latitud')}`).attr("data-element", "coordenadas-ruta"); 
+                        article.append(coordenadas);
+
+                        let altitud = $("<p>").text(`Altitud: ${rutaXML.find('coordenadas').attr('altitud')}m`).attr("data-element", "altitud-ruta"); 
+                        article.append(altitud);
+
+                        let recomendacion = $("<p>").text(`Recomendación: ${rutaXML.find('recomendacion').text()}/10`).attr("data-element", "recomendacion-ruta");
+                        article.append(recomendacion);
+
+                        article.append($("<p>").text("Referencias:"));
+                        let referencias = $("<ul>").attr("data-element", "referencias");
+                        let listaReferencias =  rutaXML.find('referencias');
+                        listaReferencias.find('referencia').each((index, ref) => {
+                            const referencia = $(ref);
+                            let enlace = $("<a>").text(`Referencia ${index+1}`).attr("href", referencia.text());
+                            let item = $("<li>").append(enlace);
+                            referencias.append(item);
+                        })
+                        article.append(referencias);
+
+                        let galeria = $("<section>").attr("data-element", "galeria");
+                        let title = $("<h4>").text("Galería de fotos");
+                        galeria.append(title);
+
+                        let seccionHitos = $("<section>").attr("data-element", "hitos");
+                        let titleHitos = $("<h4>").text("Hitos");
+                        seccionHitos.append(titleHitos);
+                        let hitos = $("<ul>").attr("data-element", "listaHitos");
+                        seccionHitos.append(hitos);
+                        let listaHitos = rutaXML.find('hitos');
+                        listaHitos.find('hito').each((index, hito) => {
+                            const hitoXML = $(hito);
+                            let nombreHito = $("<li>").text(hitoXML.find('nombre').text());
+        
+                            let caracteristicasHito = $("<ul>");
+                            let descripcion = $("<li>").text(hitoXML.find('descripcion').text());
+                            caracteristicasHito.append(descripcion);
+        
+                            let coordenadas = $("<li>").text(`Coordenadas: ${hitoXML.find('coordenadas').attr('latitud')}, ${hitoXML.find('coordenadas').attr('longitud')}`);
+                            caracteristicasHito.append(coordenadas);
+        
+                            let altitud = $("<li>").text(`Altitud: ${hitoXML.find('coordenadas').attr('altitud')}m`);
+                            caracteristicasHito.append(altitud);
+        
+                            let distanciaHitoAnterior = $("<li>").text(`Distancia al hito anterior: ${hitoXML.find('distancia-hito-anterior').text()}${hitoXML.find('distancia-hito-anterior').attr('unidad')}`);
+                            caracteristicasHito.append(distanciaHitoAnterior);
+        
+                            nombreHito.append(caracteristicasHito);
+                            hitos.append(nombreHito);
+
+                            let listaFotos = hitoXML.find('galeria-fotos');
+                            listaFotos.find('foto').each((index, foto) => {
+                                const fotoXML = $(foto);
+                                let img = $("<img>").attr("src", `xml/${fotoXML.text()}`).attr("alt", `Foto de ${hitoXML.find('nombre').text()}`);
+                                galeria.append(img);
+                            })
+                            article.append(galeria);
+                        })
+                        article.append(seccionHitos);
+
+                        $("section[data-element='rutas']").append(article);
+                    });
+                }
+            }
         }
     }
 }
